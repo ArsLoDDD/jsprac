2d82b30b96ac39addb2475ac0a1ef8b1b97bc9ab
macbook <arslod123@gmai.com>
2022-02-21 18:07:03 +0200

    JsPrac

---
+++ jjs.js
@@ -0,0 +1,39 @@
+let a = Number(prompt('Введите код от 0 до 7, чтобы '));
+if (isNaN(a) && a > 7 || a < 0) {
+    console.log('Введите корректрное число');
+}
+switch (a) {
+
+    case 0: 
+    console.log('0 - никаких прав');
+    break;
+
+    case 1: 
+    console.log('1 - только выполнение');
+    break;
+
+    case 2: 
+    console.log('2 - только запись');
+    break;
+
+    case 3: 
+    console.log('3 - выполнение и запись');
+    break;
+
+    case 4: 
+    console.log('4 -  только чтение');
+    break;
+
+    case 5: 
+    console.log('5 - чтение и выполнение');
+    break;
+
+    case 6: 
+    console.log('6 - чтение и запись');
+    break;
+
+    default: 
+    console.log('7 - чтение запись и выполнение');
+    break;
+
+}
