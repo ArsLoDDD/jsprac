0d21888caef10c81b0d39b09fe0d614ace4f6946
macbook <arslod123@gmai.com>
2022-02-22 14:42:07 +0200

    SimpleNum

--- jjs.js
+++ jjs.js
@@ -1,5 +1,8 @@
-let str = 0;
-for (let i = 1; i < 101; i++) {
-  str = str + i + ' ';
+let n = 100;
+outer:
+for (let i = 2; i <= n; i++) {
+for (let j = 2; j < i; j++) {
+if (i % j == 0) continue outer;
+  } 
+  console.log(i)
 }
-console.log(str);
