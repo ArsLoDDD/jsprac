2a235b70a96162165b51e168968b4731e684b720
macbook <arslod123@gmai.com>
2022-02-22 13:30:26 +0200

    SimpleNum

--- jjs.js
+++ jjs.js
@@ -1,29 +1,5 @@
-const a = Number(prompt('Введите код от 0 до 7, чтобы '));
-switch (a) {
-    case 0: 
-    console.log('0 - никаких прав');
-    break;
-    case 1: 
-    console.log('1 - только выполнение');
-    break;
-    case 2: 
-    console.log('2 - только запись');
-    break;
-    case 3: 
-    console.log('3 - выполнение и запись');
-    break;
-    case 4: 
-    console.log('4 -  только чтение');
-    break;
-    case 5: 
-    console.log('5 - чтение и выполнение');
-    break;
-    case 6: 
-    console.log('6 - чтение и запись');
-    break;
-    case 7: 
-    console.log('7 - чтение запись и выполнение');
-    break;
-    default:
-    console.log( 'Нет таких значений, введите код от 0 до 7' );
+let str = 0;
+for (let i = 0; i < 101; i++) {
+  str = str + i + ' ';
 }
+console.log(str);
